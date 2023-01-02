905128377331831bd4424eada983f0e1fb8013cb
Krishna Chaitanya <krishnachaitanya.reddy@iiitb.ac.in>
2023-01-02 16:58:54 +0530

    Connect to backend

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const count = 5
 const seedHex = ""
 const colorsArr = []
-const colorHexElArr = []
-const url =  "https://www.thecolorapi.com/scheme?hex=https://www.thecolorapi.com&count=" + count
+const url =  "https://www.thecolorapi.com/scheme?hex=https://www.thecolorapi.com&count=5" 
+const colorboxColorId = ["colorbox-one-color","colorbox-two-color","colorbox-three-color","colorbox-four-color","colorbox-five-color"]
+const colorboxHexId = ["colorbox-one-hex","colorbox-two-hex","colorbox-three-hex","colorbox-four-hex","colorbox-five-hex"]
+const colorboxColorEl = []
+const colorboxHexEl = []
+
+const generateColorSchemeEl = document.getElementById("generate-color-scheme")
+
+generateColorSchemeEl.addEventListener("click",generateScheme())
 
 
-function generateScheme(){
-    fetch(url)
+
+
+async function generateScheme(){
+    // console.log("Click")
+    // console.log(url)
+    await fetch(url)
         .then(res => res.json())
         .then((data) => {
             for(let i = 0;i<count;i++){
                 colorsArr[i] = data.colors[i].hex.value
             }
         })
+    // console.log(colorsArr)
+    render()
+}
 
-    console.log(colorsArr)
+
+function render(){
+    for(let i = 0;i<count;i++){
+        colorboxColorEl[i] = document.getElementById(colorboxColorId[i])
+        // .style.backgroundColor = colorsArr[i]
+        colorboxHexEl[i] = document.getElementById(colorboxHexId[i])
+        // .textContent = colorsArr[i]
+    }
+
+    for(let i = 0;i<count;i++){
+        colorboxColorEl[i].style.backgroundColor = colorsArr[i]
+        colorboxHexEl[i].innerHTML = colorsArr[i]
+        // colorboxHexEl[i].inner
+    }
+
 }
 
